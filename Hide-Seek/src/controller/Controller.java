25eae6c67d3854a92fa0bc751065d3af932ee646
bl815v <133074441+bl815v@users.noreply.github.com>
2023-11-20 15:53:27 -0500

    Validacion de casillas (falta validar claves)

--- Hide-Seek/src/controller/Controller.java
+++ Hide-Seek/src/controller/Controller.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import view.VentanaIncial;
 
 public class Controller implements ActionListener{
@@ -27,10 +29,18 @@ public class Controller implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String comando = e.getActionCommand();
+		
 		if(comando.equals("bLOGIN")) {
-			System.out.println("Inicio sesion exitosamente");
+			if(vista.getPl().getTusuario().getText().equals("")) {
+				MensajeError("El usuario ingresado es inexistente");
+			}else if (vista.getPl().getTclave().getPassword().length == 0) {
+				MensajeError("La clave ingresada es incorrecta");
+			}else {
+				System.out.println("Incio sesion exitosamente");
+			}
 		}
-		else if (comando.equals("bREGISTRATE")) {
+		
+		if (comando.equals("bREGISTRATE")) {
 			vista.getPl().setVisible(false);
 			vista.getPr().setVisible(true);
 			vista.setTitle("Registrese");
@@ -40,10 +50,58 @@ public class Controller implements ActionListener{
 			vista.setSize(710, 695);
 			vista.setMinimumSize(new Dimension(675, 580));
 		}
-		else if(comando.equals("bREGISTRAR")) {
-			System.out.println("Registrado exitosamente");
+		
+		if(comando.equals("bREGISTRAR")) {
+			boolean valido = false;
+			if(vista.getPr().getTnombre().getText().equals("")) {
+				MensajeError("Debe ingresar su nombre y apellido");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			if(vista.getPr().getTusuario().getText().equals("")) {
+				MensajeError("Debe ingresar un nombre de usuario");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			if(vista.getPr().getLista_genero().getItemAt(0).equals(" ")) {
+				MensajeError("Debe seleccionar su genero");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			if(vista.getPr().getLista_rol().getItemAt(0).equals(" ")) {
+				MensajeError("Debe seleccionar su rol");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			if (vista.getPr().getTclave().getPassword().length == 0) {
+				MensajeError("Debe ingresar una clave");
+				valido = false;
+			}else {
+				valido = true;
+			}      // corregir validacion de claves //
+			if (vista.getPr().getTclave().getPassword() != vista.getPr().getTrepetir().getPassword()) {
+				MensajeError("Las claves no coinciden");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			
+			if(vista.getPr().getTcorreo().getText().equals("")) {
+				MensajeError("Debe ingresar un correo valido");
+				valido = false;
+			}else {
+				valido = true;
+			}
+			if(valido){
+				System.out.println("Se registro exitosamente");
+			}
 		}
-		else if(comando.equals("bmenuLOGIN")) {
+		
+		if(comando.equals("bmenuLOGIN")) {
 			vista.getPl().setVisible(true);
 			vista.getPr().setVisible(false);
 			vista.setTitle("Incie sesion");
@@ -52,14 +110,20 @@ public class Controller implements ActionListener{
 			vista.adaptarPanel(vista.getPl());
 			vista.setMinimumSize(new Dimension(280, 470));
 		}
-		else if(comando.equals("LISTAgenero")) {
+		
+		if(comando.equals("LISTAgenero")) {
 			vista.getPr().getLista_genero().removeItem(" ");
 		}
-		else if(comando.equals("LISTArol")) {
+		
+		if(comando.equals("LISTArol")) {
 			vista.getPr().getLista_rol().removeItem(" ");
 		}
 	}
 	
+	public void MensajeError(String texto) {
+		JOptionPane.showMessageDialog(null, texto, "Error", JOptionPane.ERROR_MESSAGE);	
+
+	}
 	
 	
 }
